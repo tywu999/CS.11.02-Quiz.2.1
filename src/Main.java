e35b673d3a8280facb6bb24952d48f6a65a1bc53
tywu999 <tszyengw2026@student.cis.edu.hk>
2023-09-21 08:10:25 +0800

    DONE

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        int resultOne = add (3, 5);
+        System.out.println(resultOne);
+        String resultTwo = greeting("TY");
+        System.out.println(resultTwo);
+        int resultThree = add(1, 3, 5, 9);
+        System.out.println(resultThree);
+        String print = printMe("hello world");
+        System.out.println(print);
     }
 
+
+    public static int add(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
+    public static String greeting(String name){
+        return "Bonjour, " + name;
+    }
+    public static int add(int numOne, int numTwo, int numThree, int numFour){
+        int first = add(numOne, numTwo);
+        int second = add(numThree, numFour);
+        return first + second;
+    }
+    public static String printMe(String toBePrinted){
+        return toBePrinted;
+    }
 }
